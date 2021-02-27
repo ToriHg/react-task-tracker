3bc9a481a0da2a60d7f4eb13d0fffdce0566f997
ToriHg <haogechen4529@gmail.com>
2021-02-27 23:26:51 +1300

    PropTypes commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Header from './components/Header'
 
 function App() {
   return (
-    <div className="container">
+    <div className='container'>
       <Header />
-      
     </div>
   );
 }
+
 
 export default App;
