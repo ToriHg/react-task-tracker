0ac877ebe10315e44aad399e63d3e92ed12e05f5
ToriHg <haogechen4529@gmail.com>
2021-03-05 19:04:42 +1300

    toggle reminder

--- src/App.js
+++ src/App.js
@@ -28,12 +28,17 @@ function App() {
 
   //delete task
   const deleteTask = (id) =>{
-    console.log('delete',id)
+    setTasks(tasks.filter((task) => task.id !==id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task,reminder: !task.reminder} : task))
   }
   return (
     <div className='container'>
-      <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      <Header />o
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
   );
 }
